fa64d3caf7f3c90227d95b869a08850d1914a89d
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-05-20 16:36:05 +0200

    db populated

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+      $comics = config('comics');
+
+      for ($i=0; $i < count($comics); $i++) {
+        $comic = $comics[$i];
+
+        $comic_obj = new Comic();
+        $comic_obj->title = intval($comic['title']);
+        $comic_obj->description = $comic['description'];
+        $comic_obj->thumb = $comic['thumb'];
+        $comic_obj->price = $comic['price'];
+        $comic_obj->series = $comic['series'];
+        $comic_obj->sale_date = $comic['sale_date'];
+        $comic_obj->type = $comic['type'];
+        $comic_obj->save();
+      }
     }
 }
